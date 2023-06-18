04b5154a29e9d14707be7c28f25973e76a19a2a6
zkxenon <zkxenon98@gmail.com>
2023-06-18 12:45:55 +0600

    first commit

---
+++ app/src/main/java/com/example/digontoreducation/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.digontoreducation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://digontoreducation.com");
+    }
+}
